7a39aa1b833f27531443fd84b0634e33fc943a3f
Romeo <romeovs@gmail.com>
2020-01-30 10:08:24 +0100

    Make the error message more informative

--- bin/husky.js
+++ bin/husky.js
@@ -25,13 +25,15 @@ if (!(
   results[5] &&
   results[6]
 )) {
-  console.log('Valorous magus, thy commit message hast some problems.')
-  console.log('Please try the following changes:')
-  if (!results[1]) console.log('  ' + sevenrules.rule1.rule)
-  if (!results[2]) console.log('  ' + sevenrules.rule2.rule)
-  if (!results[3]) console.log('  ' + sevenrules.rule3.rule)
-  if (!results[4]) console.log('  ' + sevenrules.rule4.rule)
-  if (!results[5]) console.log('  ' + sevenrules.rule5.rule)
-  if (!results[6]) console.log('  ' + sevenrules.rule6.rule)
+  console.log('Your commit message breaks of the seven rules of commit messages')
+  console.log()
+  if (!results[1]) console.log('  Rule 1: ' + sevenrules.rule1.rule)
+  if (!results[2]) console.log('  Rule 2: ' + sevenrules.rule2.rule)
+  if (!results[3]) console.log('  Rule 3: ' + sevenrules.rule3.rule)
+  if (!results[4]) console.log('  Rule 4: ' + sevenrules.rule4.rule)
+  if (!results[5]) console.log('  Rule 5: ' + sevenrules.rule5.rule)
+  if (!results[6]) console.log('  Rule 6: ' + sevenrules.rule6.rule)
+  console.log()
+  console.log('For more info on the seven rules, see https://chris.beams.io/posts/git-commit')
   process.exit(1)
 }
